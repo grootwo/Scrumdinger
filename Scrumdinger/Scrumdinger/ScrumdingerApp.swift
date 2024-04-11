06c8f6c6bb61e72a37b58e8b624c9564e3c178e3
soaringwave <82134672+soaringwave@users.noreply.github.com>
2024-04-11 21:15:13 +0900

    Make Srcumdinger project

---
+++ Scrumdinger/Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Groo on 4/11/24.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
